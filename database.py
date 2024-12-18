7fec4be8a684faafd54c735f09bd0bbc8c6cf858
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2024-12-18 15:24:36 +0100

    refactor

--- database.py
+++ database.py
@@ -1,4 +1,6 @@
 import mysql.connector as sconnect
+from mysql.connector.abstracts import MySQLConnectionAbstract
+from mysql.connector.pooling import PooledMySQLConnection
 
 
 def get_connection():
@@ -8,6 +10,26 @@ def get_connection():
         user="root",
         password="root_password",
     )
+
+
+def register_user(
+        fname: str,
+        lname: str,
+        address: str,
+        city: str,
+        zip: int,
+        phone: str,
+        email: str,
+        password: str,
+        connection: PooledMySQLConnection | MySQLConnectionAbstract,
+):
+    cursor = connection.cursor()
+    insert_query = ("INSERT INTO book_store.members (fname, lname, address, city, zip, phone, email, password) VALUES "
+                    "(%s, %s, %s, %s, %s, %s, %s, %s)")
+    values = (fname, lname, address, city, zip, phone, email, password)
+    cursor.execute(insert_query, values)
+    connection.commit()
+    connection.close()
 
 
 if __name__ == "__main__":
@@ -20,5 +42,21 @@ if __name__ == "__main__":
         LIMIT 10
         """
     )
+
     data = cursor.fetchall()
     print(data)
+
+
+    connection = get_connection()
+    register_user(
+        fname="John",
+        lname="Doe",
+        address="123 Main St",
+        city="New York",
+        zip=10001,
+        phone="1234567890",
+        email="john.doe@example.com",
+        password="hashed_password",
+        connection=connection
+    )
+    cursor.close()

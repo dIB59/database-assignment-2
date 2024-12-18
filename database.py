7e43072117f46005e3ec97743d7be5c01247a7a2
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2024-12-18 22:17:27 +0100

    reformat

--- database.py
+++ database.py
@@ -13,19 +13,21 @@ def get_connection():
 
 
 def register_user(
-        fname: str,
-        lname: str,
-        address: str,
-        city: str,
-        zip: int,
-        phone: str,
-        email: str,
-        password: str,
-        connection: PooledMySQLConnection | MySQLConnectionAbstract,
+    fname: str,
+    lname: str,
+    address: str,
+    city: str,
+    zip: int,
+    phone: str,
+    email: str,
+    password: str,
+    connection: PooledMySQLConnection | MySQLConnectionAbstract,
 ):
     cursor = connection.cursor()
-    insert_query = ("INSERT INTO book_store.members (fname, lname, address, city, zip, phone, email, password) VALUES "
-                    "(%s, %s, %s, %s, %s, %s, %s, %s)")
+    insert_query = (
+        "INSERT INTO book_store.members (fname, lname, address, city, zip, phone, email, password) VALUES "
+        "(%s, %s, %s, %s, %s, %s, %s, %s)"
+    )
     values = (fname, lname, address, city, zip, phone, email, password)
     cursor.execute(insert_query, values)
     connection.commit()
@@ -46,7 +48,6 @@ if __name__ == "__main__":
     data = cursor.fetchall()
     print(data)
 
-
     connection = get_connection()
     register_user(
         fname="John",
@@ -57,6 +58,6 @@ if __name__ == "__main__":
         phone="1234567890",
         email="john.doe@example.com",
         password="hashed_password",
-        connection=connection
+        connection=connection,
     )
     cursor.close()

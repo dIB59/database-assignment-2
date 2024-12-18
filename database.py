2249842185b8f47acdd166f337bb218b1a921e87
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2024-12-18 12:44:23 +0100

    impl get connection

--- database.py
+++ database.py
@@ -1,3 +1,22 @@
-def connect(url: str):
+import mysql.connector as sconnect
 
-    return None
+
+def get_connection(url: str = "127.0.0.1"):
+    return sconnect.connect(
+        host="localhost",
+        port="3306",
+        user="root",
+        password="root_password",
+    )
+
+
+if __name__ == "__main__":
+    connection = cxt = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT * FROM book_store.books
+        LIMIT 10
+        """)
+    data = cursor.fetchall()
+    print(data)
